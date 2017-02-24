895e39c947bc806b36a0ffdc9a53ef1122a6f964
alejandro <alejandro@doctoralia.com>
2017-02-24 16:48:19 +0100

    Initial commit

---
+++ JsonG.NET/JsonG.NET.Core/JsonG.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonG.NET.Core
+{
+    public class JsonG
+    {
+        public static string SerializeObject(object @object)
+        {
+            return JsonConvert.SerializeObject(@object);
+        }
+    }
+}
